ff867aa0095fc4978a2d91a4fd0bfe22182abf20
adtydwpr <114389480+adtydwpr@users.noreply.github.com>
2022-10-23 12:32:11 +0700

    Create Pelajar.java

---
+++ Pelajar.java
@@ -0,0 +1,11 @@
+public class Pelajar {
+    String nama;
+
+    Pelajar(String nama){
+        this.nama = nama;
+    }
+
+    void info(){
+        System.out.println("Nama Pelajar \t : " + this.nama);
+    }
+}
